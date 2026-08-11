53353e5604f2368fe723bf9009dc091c7c4159ce
BabaWadj <wadj.baba@uam.edu.sn>
2026-08-11 22:18:08 +0000

    refactor: improve Client encapsulation by using setter methods and robust key-based dictionary management

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ from zone import Zone
 
 class Client:
     def __init__(self, id, nom):
-        self.__id = id
-        self.__nom = nom
+        self.set_id(id)
+        self.set_nom(nom)
         self.__adresses = {}
         self.__materiels = {}
 
@@ -18,10 +18,7 @@ class Client:
             raise TypeError("L'identifiant doit être un entier.")
 
     def get_nom(self):
-        if isinstance(self.__nom, str):
-            return self.__nom
-        else:
-            raise TypeError("Le nom doit être une chaîne de caractères.")
+        return self.__nom
 
     def set_nom(self, nom):
         if isinstance(nom, str):
@@ -36,23 +33,23 @@ class Client:
         if not self.__adresses:
             return "Aucune adresse enregistrée."
         else:
-            return "\n".join(self.__adresses[adresse] for adresse in self.__adresses)
+            return "\n".join(adresse for adresse in self.__adresses.values())
 
     def add_adresse(self, adresse):
         if not isinstance(adresse, Zone):
             raise TypeError("L'adresse doit être un objet de type Zone.")
-        elif adresse in self.__adresses:
+        elif adresse.get_id() in self.__adresses.keys():
             raise ValueError("L'adresse existe déjà dans la liste des adresses du client.")
         else:
-            self.__adresses.update({adresse.__id: adresse})
+            self.__adresses.update({adresse.get_id(): adresse})
 
     def remove_adresse(self, adresse):
         if not isinstance(adresse, Zone):
             raise TypeError("L'adresse doit être un objet de type Zone.")
-        elif adresse not in self.__adresses:
+        elif adresse.get_id() not in self.__adresses.keys():
             raise ValueError("L'adresse n'existe pas dans la liste des adresses du client.")
         else:
-            del self.__adresses[adresse.__id]
+            del self.__adresses[adresse.get_id()]
 
     def get_materiels(self):
         return dict(self.__materiels)
@@ -61,22 +58,22 @@ class Client:
         if not self.__materiels:
             return "Aucun matériel enregistré."
         else:
-            return "\n".join([f"{self.__materiels[materiel].marque} {self.__materiels[materiel].modele} ({self.__materiels[materiel].id_materiel})\n" for materiel in self.__materiels])
+            return "\n".join([f"{self.__materiels[id_materiel]}\n" for id_materiel in self.__materiels.keys()])
 
     def add_materiel(self, materiel):
         if not isinstance(materiel, Materiel):
             raise TypeError("Le matériel doit être un objet de type Materiel.")
-        elif materiel in self.__materiels:
+        elif materiel.get_id_materiel() in self.__materiels.keys():
             raise ValueError("Le matériel existe déjà dans la liste du matériel du client.")
         else:
-            self.__materiels.update({materiel.id_materiel: materiel})
+            self.__materiels.update({materiel.get_id_materiel(): materiel})
 
     def remove_materiel(self, materiel):
         if not isinstance(materiel, Materiel):
             raise TypeError("Le matériel doit être un objet de type Materiel.")
-        elif materiel not in self.__materiels:
+        elif materiel.get_id_materiel() not in self.__materiels.keys():
             raise ValueError("Le matériel n'existe pas dans la liste du matériel du client.")
         else:
-            del self.__materiels[materiel.id_materiel]
+            del self.__materiels[materiel.get_id_materiel()]
 
     
